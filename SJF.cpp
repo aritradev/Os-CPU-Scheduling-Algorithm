aa8cffc6831b77e5d609f2e47e85556dd3278237
Aritra dev <147065581+aritradev@users.noreply.github.com>
2026-05-05 02:25:30 +0600

    Refactor SJF scheduling algorithm and output functions

--- SJF.cpp
+++ SJF.cpp
@@ -5,10 +5,12 @@ struct process
     string id;
     int arival;
     int burst;
-    int exi;
+    int exit;
     int turn;
     int wait;
 };
+vector<string>gid;
+vector<int>g;
 bool cmp(const process &p1,const process &p2){
     if(p1.arival==p2.arival)return p1.id<p2.id;
     return p1.arival<p2.arival;
@@ -16,73 +18,71 @@ bool cmp(const process &p1,const process &p2){
 void calculate(vector<process>&v){
     sort(v.begin(),v.end(),cmp);
     int n = v.size();
-    vector<process>result;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-    int i =0,cur=0;
-    while(result.size()<n){
-       while(i<n && cur>=v[i].arival){
-        pq.push({v[i].burst,i});
-        i++;
-       }
+    int cur=0,i=0,complete=0;
+    string last;
+    priority_queue<pair<int,int>, vector<pair<int,int>> ,greater<pair<int,int>>>pq;
+
+    while(complete<n){
+        while(i<n && cur>=v[i].arival){
+            pq.push({v[i].burst,i});
+            i++;
+        }
         if(pq.empty()){
+            if(gid.empty()||last!="Idle"){
+                last="Idle";
+                gid.push_back(last);
+                g.push_back(cur);
+            }
             cur=v[i].arival;
         }
         else{
-            int j = pq.top().second;
+            int j= pq.top().second;
             pq.pop();
+
+            if(last!=v[j].id){
+                last=v[j].id;
+                gid.push_back(last);
+                g.push_back(cur);
+            }
+            complete++;
             cur+=v[j].burst;
-            v[j].exi=cur;
+            v[j].exit= cur;
             v[j].turn=cur-v[j].arival;
             v[j].wait=v[j].turn-v[j].burst;
-            result.push_back(v[j]);
+
         }
     }
-    v=result;
+    g.push_back(cur);
 }
-void gant(vector<process>&v){
-    int cur=0;
-    for(auto u:v){
-        if(cur<u.arival){
-            cout<<"Idle\t|\t";
-        }
-        cur=u.exi;
-        cout<<u.id<<"\t|\t";
-    }
-     cout<<"\n";
-    cur=0;
-    cout<<cur<<"   \t";
-    for(auto u:v){
-        if(cur<u.arival){
-            cout<<u.arival<<"\t\t";
-        }
-        cur=u.exi;
-        cout<<cur<<"   \t\t";
-    }
-    cout<<"\n\n";
+void gant(){
+    for(auto u:gid)cout<<u<<"\t|\t";
+    cout<<endl;
+    for(auto u:g)cout<<u<<"\t\t";
+    cout<<endl<<endl;
+    
 }
+
 
 void printt(vector<process>&v){
     cout<<"Id\t"<<"AT\t"<<"BT\t ET\t TAT\t WA\t\n\n";
     for(auto &u:v){
-        cout<<u.id<<"\t"<<u.arival<<"\t"<<u.burst<<"\t"<<u.exi<<"\t"<<u.turn<<"\t"<<u.wait<<"\n";
+        cout<<u.id<<"\t"<<u.arival<<"\t"<<u.burst<<"\t"<<u.exit<<"\t"<<u.turn<<"\t"<<u.wait<<"\n";
     }
 }
 int main(){
-    int n;cin>>n;
-    vector<process>v(n);
-    for(auto &u:v){
-        cout<<"Id: ";
-        cin>>u.id;
-        cout<<"\n";
-        cout<<"Arival: ";
-        cin>>u.arival;
-        cout<<"\n";
-        cout<<"Burst Time: ";
-        cin>>u.burst;
-        cout<<"\n";
-
+    int n; 
+    
+    cin >> n;
+    vector<process> v(n);
+    
+   
+    for(auto &u : v){
+        cin >> u.id >> u.arival >> u.burst;
     }
+    
     calculate(v);
-    gant(v);
-   printt(v);
+    gant();
+    printt(v);
+
+    return 0;
 }
